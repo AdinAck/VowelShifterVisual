eb0399c49236fcb182d7f6f9041640148f598680
Adin Ackerman <adinackerman@gmail.com>
2022-04-16 15:38:59 -0700

    Initial Commit

---
+++ VowelShifterVisual/VowelShifterVisualApp.swift
@@ -0,0 +1,17 @@
+//
+//  VowelShifterVisualApp.swift
+//  VowelShifterVisual
+//
+//  Created by Adin Ackerman on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct VowelShifterVisualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
